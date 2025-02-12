e24773de18a04f47aa961afe5817198a46e62bee
Nishant kumar <nishant927472@gmail.com>
2025-02-12 17:09:13 +0530

    50% done

---
+++ Portfolio-website/src/App.jsx
@@ -0,0 +1,20 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Navbar from './components/Navbar'
+import './App.css'
+import Name from './components/Name'
+import About from './components/About'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Name />
+      <hr />
+      <About/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ Portfolio-website/src/components/Name.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Name.css'
+import { useEffect,useState } from 'react';
+export default function Name() {
+  
+  return (
+    <div className="name">
+      <div className='name_div'>
+        <span> I'm NISHANT</span>
+      </div>
+      
+
+    </div>
+  )
+}
